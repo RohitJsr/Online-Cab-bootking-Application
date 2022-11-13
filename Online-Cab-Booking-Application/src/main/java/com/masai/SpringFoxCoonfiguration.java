02efcecb384c59e68b937af9c9ecfdc68b8287a9
Anurag-shekawat <anuragshekhawat806@gmail.com>
2022-11-13 12:49:44 +0530

    some changes for swagger

--- Online-Cab-Booking-Application/src/main/java/com/masai/SpringFoxCoonfiguration.java
+++ Online-Cab-Booking-Application/src/main/java/com/masai/SpringFoxCoonfiguration.java
@@ -1,24 +1,40 @@
 package com.masai;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SpringFoxCoonfiguration {
 
-	  @Bean
-	    public Docket api() { 
-	        return new Docket(DocumentationType.SWAGGER_2)  
-	          .select()                                  
-	          .apis(RequestHandlerSelectors.any())              
-	          .paths(PathSelectors.any())                          
-	          .build();                                           
-	    }
-	
-	
+//	  @Bean
+//	    public Docket api() { 
+//	        return new Docket(DocumentationType.SWAGGER_2)  
+//	          .select()                                  
+//	          .apis(RequestHandlerSelectors.any())              
+//	          .paths(PathSelectors.any())                          
+//	          .build();                                           
+//	    }
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.masai")).build().apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo("Online Cab Booking Application", "Api for cab booking application", "1.0", "Free to use",
+				new springfox.documentation.service.Contact(null, null, null), "Github repo link",
+				"https://github.com/Aman103767/fretful-peace-608", Collections.emptyList());
+
+	}
+
 }
